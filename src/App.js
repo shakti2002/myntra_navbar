f4b7aac4936df88465e4b064f791a71621d05ed3
shakti2002 <shakti25io054@satiengg.in>
2024-06-17 22:56:28 +0530

    testing

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+    
+    
+
+    return (
+        <div className="App">
+            <h1>
+                practice-deploying in vercel
+            </h1>
+        </div>
+    );
+}
+
+export default App;
